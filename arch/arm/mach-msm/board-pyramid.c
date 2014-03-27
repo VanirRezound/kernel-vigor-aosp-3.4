a0b70af5644e12e565f1826dd41a1f2609704642
Sultanxda <sultanxda@gmail.com>
2014-03-27 11:34:06 -0700

    pyramid: Set Ondemand 2-phase freq to 1080MHz
    To avoid unnecessary hotplug events by ASMP.

--- arch/arm/mach-msm/board-pyramid.c
+++ arch/arm/mach-msm/board-pyramid.c
@@ -4940,7 +4940,7 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 	msm8x60_init_mmc();
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
-	set_two_phase_freq(1134000);
+	set_two_phase_freq(1080000);
 #endif
 
 	msm8x60_init_camera();
